31f8aea297e4b7dfddb73cdf8d62fc924375cd4e
Peter Borkuti <peter.borkuti@liferay.com>
2015-01-21 15:09:28 +0100

    missing group ids cleaner hook for 6210

---
+++ hooks/6210/missing-group-ids/README.md
@@ -0,0 +1,112 @@
+# Delete entries where the group for it's groupid no longer exists
+
+## The MSSQL script which lists the tables where there is groupid which Group was deleted somehow
+
+### The SQL
+
+```SQL
+Use uea_after
+
+declare @table_name varchar(50)
+declare @count1 int
+declare @sql1 nvarchar(4000)
+declare @count2 int
+declare @sql2 nvarchar(4000)
+declare @params nvarchar(4000)
+declare @where nvarchar(1000)
+declare @sql3 nvarchar(4000)
+declare @params3 nvarchar(4000)
+declare @out3 nvarchar(4000)
+
+declare tables cursor for 
+	Select TABLE_NAME From INFORMATION_SCHEMA.COLUMNS Where column_name = 'groupId' order by TABLE_NAME;
+
+select @where = N' WHERE groupId NOT IN (SELECT DISTINCT groupId FROM [Group_])'
+select @params = N'@cnt int OUTPUT'
+select @params3 = N'@s nvarchar(4000) OUTPUT'
+
+open tables
+fetch next from tables into @table_name
+
+while @@FETCH_STATUS = 0
+begin
+	-- count of missing groupids
+   select @sql1 = N'SELECT @cnt = COUNT([groupId]) FROM ' +
+				 quotename(@table_name) + @where
+				 
+	-- count distinct of missing groupids
+	select @sql2 = N'SELECT @cnt = COUNT(distinct [groupId]) FROM ' +
+				 quotename(@table_name) + @where
+
+	-- writing the missing groupids in one line separated by comma
+	select @sql3 = N'SET NOCOUNT ON;' +
+					N'DECLARE @groupids TABLE(groupId bigint NULL);INSERT INTO @groupids ' +
+					N'SELECT distinct [groupId] FROM ' + quotename(@table_name) + @where + '; ' +
+					N'select  @s = (select convert(varchar(10), groupId) + ' + ''',''' + 
+					' as ''data()'' from @groupids order by groupid for xml path(''''))'
+
+	exec sp_executesql @sql1, @params, @cnt = @count1 OUTPUT
+
+	if @count1 > 0
+	begin
+		exec sp_executesql @sql2, @params, @cnt = @count2 OUTPUT
+		exec sp_executesql @sql3, @params3, @s = @out3 OUTPUT
+		-- |TABLE NAME|COUNT MISSINg GRIDS|COUNT DISTINCT MISSING GRIDS|MISSING GRIDS
+		print @table_name + '|' + convert(varchar(10), @count1) + '|' + convert(varchar(10), @count2) + '|' + @out3
+	end
+
+   fetch next from tables into @table_name	
+end
+
+close tables
+deallocate tables
+```
+
+### The output
+
+```
+AssetEntry|4838|40|10648, 10988, 15267, 16086, 16129, 16151, 43679, 51040, 78021, 86317, 95351, 101394, 135964, 143628, 145211, 148956, 153957, 165890, 168570, 178813, 183156, 231469, 231489, 243980, 301599, 311379, 314284, 505234, 982188, 1001111, 1001269, 1061102, 1095198, 1393081, 1419580, 1555200, 3084030, 3306541, 3998811, 4741899,
+AssetTag|43|7|86317, 95351, 101394, 168570, 183156, 1001111, 1001269,
+AssetVocabulary|10|8|10988, 51040, 78021, 95351, 178813, 178819, 311379, 982188,
+DLFileEntryType|1|1|0,
+DLFileRank|6|3|10988, 51040, 86317,
+DLFolder|7|1|982188,
+JournalArticleResource|10|3|243980, 311379, 982188,
+JournalContentSearch|4|1|982188,
+KaleoAction|2|1|0,
+KaleoDefinition|1|1|0,
+KaleoNode|4|1|0,
+KaleoNotification|3|1|0,
+KaleoNotificationRecipient|1|1|0,
+KaleoTask|2|1|0,
+KaleoTaskAssignment|9|1|0,
+KaleoTransition|4|1|0,
+Layout|10|1|982188,
+LayoutFriendlyURL|10|1|982188,
+LayoutSet|4|2|982188, 999117,
+MBDiscussion|80|8|0, 10988, 51040, 78021, 86317, 95351, 101394, 982188,
+MBMessage|118|10|10648, 10988, 16151, 51040, 78021, 86317, 95351, 101394, 183156, 982188,
+MBThread|118|10|10648, 10988, 16151, 51040, 78021, 86317, 95351, 101394, 183156, 982188,
+SocialActivity|59|3|16129, 153957, 301599,
+```
+
+The first column is the name of table, the second is the number of orphaned groups, the third is the number of dictinct orpahned groups
+The last is the groupids separated with comma.
+
+## The portal.properties
+```
+# dont.delete = false : it will do the cleaning
+# dont.delete = true : it will not do the cleaning just a dry-run. Try this first. If everything ok, set it to true and redeploy the hook
+dont.delete=false
+
+# the rows of the tables will processed by chunks to avoid out of memor errors. However, the more the chunks, the slower the run
+# we tested against the database with cleaner.chunks = 3, it runs, so probably it will be OK.
+cleaner.chunks = 3;
+
+# the models and packages for clearing. Model,package;Mdel,package;etc. Beware of the "."! It is importan at the end of the package.
+# cleaner.entries=AssetCategory,com.liferay.portlet.asset.
+cleaner.entries = AssetCategory,com.liferay.portlet.asset.;AssetEntry,com.liferay.portlet.asset.;AssetTag,com.liferay.portlet.asset.;AssetVocabulary,com.liferay.portlet.asset.;DLFileEntryType,com.liferay.portlet.documentlibrary.;DLFileRank,com.liferay.portlet.documentlibrary.;DLFolder,com.liferay.portlet.documentlibrary.;JournalArticleResource,com.liferay.portlet.journal.;JournalContentSearch,com.liferay.portlet.journal.;Layout,com.liferay.portal.;LayoutSet,com.liferay.portal.;MBMessage,com.liferay.portlet.messageboards.;MBThread,com.liferay.portlet.messageboards.;SocialActivity,com.liferay.portlet.social.;DDMContent,com.liferay.portlet.dynamicdatamapping.;DLFileEntry,com.liferay.portlet.documentlibrary.;DLFileVersion,com.liferay.portlet.documentlibrary.;JournalArticle,com.liferay.portlet.journal.
+
+# do not modify this
+application.startup.events=com.liferay.groupidcleaner.startupaction.GroupidCleaner
+```
---
+++ hooks/6210/missing-group-ids/missing-groupid-cleaner-hook/docroot/WEB-INF/src/com/liferay/groupidcleaner/startupaction/GroupidCleaner.java
@@ -0,0 +1,227 @@
+
+package com.liferay.groupidcleaner.startupaction;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.SimpleAction;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+
+public class GroupidCleaner extends SimpleAction {
+
+	public static final String CLASSNAME = GroupidCleaner.class.getSimpleName();
+
+	public GroupidCleaner() {
+
+		super();
+	}
+
+	protected int cleanChunk(
+		int start, int end, int count, String methodGetEntries,
+		String classModel, String methodDeleteEntry, String classLocalSUFQDN,
+		Class service, boolean dontDelete)
+		throws Exception {
+
+		_log.debug("cleanChunk: from " + start + " to " + end + ", count: " + count);
+
+		List entries = null;
+		int deletedCount = 0;
+		int failedCount = 0;
+
+		try {
+			_log.debug("invoking " + methodGetEntries + " on " +
+				classLocalSUFQDN);
+			java.lang.reflect.Method method =
+				service.getMethod(methodGetEntries, int.class, int.class);
+			entries = (List) method.invoke(null, start, Math.min(count, end));
+		}
+		catch (Exception e) {
+			_log.debug("Could not find entries.");
+			e.printStackTrace();
+			return deletedCount;
+		}
+
+		_log.debug("Count of selected items: " + entries.size());
+		_log.debug("");
+
+		long groupId = 0;
+
+		long id = 0;
+
+		for (Object entry : entries) {
+
+			// get ID
+			BaseModel bm = (BaseModel) entry;
+			id = (Long) (bm.getPrimaryKeyObj());
+			// _log.debug("Checking id: " + id);
+
+			// get groupId
+
+			Class model = Class.forName(classModel);
+			java.lang.reflect.Method getGroupMethod =
+				model.getMethod("getGroupId");
+			groupId = (Long) getGroupMethod.invoke(entry);
+
+			if (groupId > 0) {
+				try {
+					if (GroupLocalServiceUtil.fetchGroup(groupId) == null) {
+						_log.debug("Missing group id:" + groupId);
+						if (!dontDelete) {
+							try {
+								java.lang.reflect.Method method =
+									service.getMethod(
+										methodDeleteEntry, long.class);
+								method.invoke(null, id);
+								deletedCount++;
+							}
+							catch (Exception e) {
+								// TODO Auto-generated catch block
+								_log.error("can not delete entry:" + id, e);
+								failedCount++;
+							}
+						}
+					}
+				}
+				catch (SystemException e) {
+					// TODO Auto-generated catch block
+					_log.error("Error when getting groupid:" + groupId);
+				}
+			}
+		}
+
+		_log.debug("");
+		_log.debug("Number of deleted entries:" + deletedCount);
+		_log.debug("Number of failed deletion of entries:" + failedCount);
+		_log.debug("");
+
+		return deletedCount;
+
+	}
+
+	private void doDelete(
+		String className, String packageName, boolean dontDelete, int chunks)
+		throws Exception {
+
+		String classNamePlural = "";
+		if (className.endsWith("y")) {
+			classNamePlural =
+				className.substring(0, className.length() - 1) + "ies";
+		}
+		else {
+			classNamePlural = className + "s";
+		}
+
+		String classLocalSU = className + "LocalServiceUtil";
+		String methodCount = "get" + classNamePlural + "Count";
+		String methodGetEntries = "get" + classNamePlural;
+		String methodDeleteEntry = "delete" + className;
+		String modelPackage = packageName + "model";
+		String classModel = modelPackage + "." + className + "Model";
+
+		String servicePackageName = packageName + "service";
+		String classLocalSUFQDN = servicePackageName + "." + classLocalSU;
+
+		Class<?> service = null;
+		try {
+			service = Class.forName(classLocalSUFQDN);
+		}
+		catch (ClassNotFoundException e1) {
+			_log.error("class " + classLocalSUFQDN + " not found");
+			return;
+		}
+
+		int count = 0;
+
+		try {
+			_log.debug("invoking " + methodCount + " on " + classLocalSUFQDN);
+			java.lang.reflect.Method method = service.getMethod(methodCount);
+			count = (Integer) method.invoke(null);
+		}
+		catch (Exception e) {
+			_log.debug("Could not call method:" + methodCount);
+			e.printStackTrace();
+			return;
+		}
+
+		int chunkCount = (count / chunks) + 1;
+
+		int j = 1;
+		for (int i = 0; i < count; i += chunkCount) {
+			_log.debug(className + " chunk:" + j);
+
+			int deleted = cleanChunk(
+				i, i + chunkCount, count, methodGetEntries, classModel,
+					methodDeleteEntry, classLocalSUFQDN, service, dontDelete);
+
+			count -= deleted;
+
+			i -= deleted;
+
+			j++;
+		}
+
+		return;
+	}
+
+	@Override
+	public void run(String[] arg0)
+		throws ActionException {
+
+		boolean dontDelete = true;
+
+		dontDelete = GetterUtil.getBoolean(PropsUtil.get("dont.delete"), true);
+
+		String entryprop =
+			GetterUtil.getString(PropsUtil.get("cleaner.entries"), "");
+
+		if (Validator.isNull(entryprop)) {
+			_log.error("cleaner.properties is empty. There is no table to clean");
+			return;
+		}
+
+		_log.debug("cleaner.entries:" + entryprop);
+
+		int chunks = GetterUtil.getInteger(PropsUtil.get("cleaner.chunks"), 0);
+
+		String[] entries = StringUtil.split(entryprop, ";");
+
+		_log.debug(CLASSNAME + " hook started");
+		_log.debug("");
+
+		for (String entry : entries) {
+			String[] parts = StringUtil.split(entry, ",");
+
+			if (parts.length != 2) {
+				_log.error("bad entry in cleaner.properties:" + entry);
+				continue;
+			}
+
+			_log.debug("start deleting entries with missing groupid from " +
+				parts[0]);
+
+			String className = parts[0];
+			String packageName = parts[1];
+
+			try {
+				doDelete(className, packageName, dontDelete, chunks);
+			}
+			catch (Exception e) {
+				_log.error(e, e);
+			}
+		}
+
+		_log.debug(CLASSNAME + " hook ended");
+
+	}
+
+	private static Logger _log = Logger.getLogger(GroupidCleaner.class);
+
+}
